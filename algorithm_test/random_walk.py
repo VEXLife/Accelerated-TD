50f7ffbbaae2fdfa500893548d2c433fd80d63b0
Midden Vexu <36587232+VEXLife@users.noreply.github.com>
2021-10-02 23:08:12 +0800

    Update random_walk.py

--- algorithm_test/random_walk.py
+++ algorithm_test/random_walk.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 import sys
 
 sys.path.append("..")
-from ATD_cn import TDAgent, SVDATDAgent, PlainATDAgent
+from ATD_cn import TDAgent, SVDATDAgent,SVDLRATDAgent, PlainATDAgent
 
 N = 7
 v_true = np.arange(N)/(N-1)
@@ -51,16 +51,23 @@ def play_game(agent, episodes=100, iterations=100):
     return np.mean(records, axis=0)
 
 
-plt.figure(dpi=120)
+plt.figure(dpi=120,figsize=(8,6))
 
-plt.plot(play_game(agent=TDAgent(lr=0.02, lambd=0, observation_space_n=7,
-                                 action_space_n=2), episodes=1000, iterations=10), label="TD(0), $\\alpha=0.02$")
-plt.plot(play_game(agent=SVDATDAgent(alpha=0.02,k=100,eta=1e-4,lambd=0,observation_space_n=7,action_space_n=2),episodes=1000,iterations=10),label="SVDATD(0), $\\alpha=0.02$, $\\eta=1\\times10^{-4}$, $r=100$")
-plt.plot(play_game(agent=PlainATDAgent(alpha=0.02, eta=1e-4, lambd=0, observation_space_n=7, action_space_n=2),
-                   episodes=1000, iterations=10), label="PlainATD(0), $\\alpha=0.02$, $\\eta=1\\times10^{-4}$")
+plt.plot(play_game(agent=TDAgent(lr=0.02, lambd=0.5, observation_space_n=7,
+                                 action_space_n=2), episodes=1000, iterations=10), label="TD(0.5), $\\alpha=0.02$")
+plt.plot(play_game(agent=SVDLRATDAgent(alpha=0.02, k=50, eta=1e-4, lambd=0.5, observation_space_n=7, action_space_n=2),
+                   episodes=1000, iterations=10), label="SVDLRATD(0.5), $\\alpha=0.02$, \n$\\eta=1\\times10^{-4}$, $r=50$, Accuracy First")
+plt.plot(play_game(agent=SVDLRATDAgent(alpha=0.02, k=50, eta=1e-4, lambd=0.5, observation_space_n=7, action_space_n=2, w_update_emphasizes="complexity"),
+                   episodes=1000, iterations=10), label="SVDLRATD(0.5), $\\alpha=0.02$, \n$\\eta=1\\times10^{-4}$, $r=50$, Complexity First")
+plt.plot(play_game(agent=SVDATDAgent(alpha=0.02, eta=1e-4, lambd=0.5, observation_space_n=7, action_space_n=2),
+                   episodes=1000, iterations=10), label="SVDATD(0.5), $\\alpha=0.02$, \n$\\eta=1\\times10^{-4}$, Accuracy First")
+plt.plot(play_game(agent=SVDATDAgent(alpha=0.02, eta=1e-4, lambd=0.5, observation_space_n=7, action_space_n=2, w_update_emphasizes="complexity"),
+                   episodes=1000, iterations=10), label="SVDATD(0.5), $\\alpha=0.02$, \n$\\eta=1\\times10^{-4}$, Complexity First")
+plt.plot(play_game(agent=PlainATDAgent(alpha=0.02, eta=1e-4, lambd=0.5, observation_space_n=7, action_space_n=2),
+                   episodes=1000, iterations=10), label="PlainATD(0.5), $\\alpha=0.02$, $\\eta=1\\times10^{-4}$")
 plt.legend()
 plt.title("Random Walking")
 plt.xlabel("Episode")
 plt.ylabel("RMSE")
-plt.ylim(0,10)
+plt.ylim(0, 10)
 plt.savefig("../figures/random_walking.png", format="png")
